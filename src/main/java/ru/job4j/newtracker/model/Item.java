abb5d44d4f698fa8da092514b515fff371cf0298
Sergey Morozov <moroz.future@gmail.com>
2021-01-12 17:34:31 +0400

    Добавил JavaDocs.

--- src/main/java/ru/job4j/newtracker/model/Item.java
+++ src/main/java/ru/job4j/newtracker/model/Item.java
@@ -3,29 +3,59 @@ package ru.job4j.newtracker.model;
 import javax.persistence.*;
 import java.util.Objects;
 
+/**
+ * Класс преставляющий заявку.
+ *
+ * @author Sergey Morozov (morozov.java.job@gmail.com)
+ * @version 1.0
+ */
+
 @Entity
 @Table(name = "items")
 public class Item {
 
+    /**
+     * Идентификационный номер заявки.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    /**
+     * Имя заявки.
+     */
     private String name;
 
+    /**
+     * Конструктор класса Item.
+     * @param name - имя заявки.
+     */
     public static Item of(String name) {
         Item item = new Item();
         item.name = name;
         return item;
     }
 
+    /**
+     * Возвращает ID заявки.
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Устанавливает ID заявки.
+     * @param id - id заявки.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Возвращает имя заявки.
+     * @return имя заявки.
+     */
     public String getName() {
         return name;
     }
