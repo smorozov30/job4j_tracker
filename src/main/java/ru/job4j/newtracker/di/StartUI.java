ff8796f947a5be71469540f40422be05dd286f63
Sergey Morozov <moroz.future@gmail.com>
2021-01-26 11:35:11 +0400

    @Autowired. Внедрение зависимости [#428600]

--- src/main/java/ru/job4j/newtracker/di/StartUI.java
+++ src/main/java/ru/job4j/newtracker/di/StartUI.java
@@ -1,15 +1,17 @@
 package ru.job4j.newtracker.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartUI {
 
+    @Autowired
     private Store store;
     private ConsoleInput consoleInput;
 
-    public StartUI(Store store, ConsoleInput consoleInput) {
-        this.store = store;
+    @Autowired
+    public void setConsoleInput(ConsoleInput consoleInput) {
         this.consoleInput = consoleInput;
     }
 
