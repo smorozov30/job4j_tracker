ddd844ed8f886fefcc2579cda654c0ea9adf6539
Sergey Morozov <moroz.future@gmail.com>
2021-01-26 11:22:51 +0400

    Сканирование проекта [#428602]

--- src/main/java/ru/job4j/newtracker/di/ConsoleInput.java
+++ src/main/java/ru/job4j/newtracker/di/ConsoleInput.java
@@ -1,7 +1,10 @@
 package ru.job4j.newtracker.di;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Scanner;
 
+@Component
 public class ConsoleInput {
     public String input() {
         return "From ConsoleInput: " + new Scanner(System.in).nextLine();
--- src/main/java/ru/job4j/newtracker/di/StartUI.java
+++ src/main/java/ru/job4j/newtracker/di/StartUI.java
@@ -1,5 +1,8 @@
 package ru.job4j.newtracker.di;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class StartUI {
 
     private Store store;
